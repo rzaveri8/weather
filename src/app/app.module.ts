b39f08bc4f02ea4b960fa0e8fc634a463c179fb9
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2018-01-20 18:05:29 +0300

    css edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { WeatherService } from './services/weather.service';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CityComponent } from './components/city/city.component';
@@ -10,10 +9,6 @@ import { CityBasicComponent } from './components/city-basic/city-basic.component
 import { CityAddComponent } from './components/city-add/city-add.component';
 import { CityDetailComponent } from './components/city-detail/city-detail.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  { path: 'city-detail/:city', component: CityDetailComponent, data: { weatherData: 'weatherData' } }
-];
 
 @NgModule({
   declarations: [
@@ -25,9 +20,6 @@ const appRoutes: Routes = [
     PageNotFoundComponent
   ],
   imports: [
-    RouterModule.forRoot(
-      appRoutes
-    ),
     BrowserModule,
     HttpClientModule
   ],
